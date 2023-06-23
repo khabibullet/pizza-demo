64b0253a218b16d010b27015a72a1378644703d1
Irek Khabibullin <irek.rifson@gmail.com>
2023-06-23 12:57:47 +0300

    fix json

--- pizza-demo/Services/MenuProvider.swift
+++ pizza-demo/Services/MenuProvider.swift
@@ -14,7 +14,7 @@ class MenuProvider {
     
     let realm = try! Realm()
     
-    static let menuURL = URL(string: "")!
+    static let menuURL = URL(string: "https://raw.githubusercontent.com/khabibullet/pizza-demo/master/contents/pizza-demo-data.json")!
     
     private init() {}
     
